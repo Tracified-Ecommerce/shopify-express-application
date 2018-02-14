c2f2e02d01389bbf2984e2e832ec1d06e425c47d
DAMMA <dammakaru@gmail.com>
2018-02-14 06:49:21 +0530

    ts errors

--- src/tracified/services.ts
+++ src/tracified/services.ts
@@ -75,6 +75,7 @@ export class Services implements IServices {
             };
 
             request(options).then((data: any) => {
+                console.log("getModaData returns :"+data);
                 const type: string = typeof data;
                 resolve(data);
             });
