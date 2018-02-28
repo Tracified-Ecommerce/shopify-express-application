60a6adf06aef7850e7c0131806850c11c7f51c3c
dammakaru <dammakaru@gmail.com>
2018-02-28 15:00:40 +0530

    figuring out response structure2

--- src/shopify/routes/tracified.ts
+++ src/shopify/routes/tracified.ts
@@ -26,10 +26,11 @@ router.all("/*", (req: IRequest, res: Response, next: NextFunction) => {
 router.get("/item-list", (req: IRequest & Request, res: Response) => {
     tracifiedServices.getTracifiedItemList(req.session.shop.tracified_token).then((data: any) => {
         console.log(data);
+        const dataJSON = JSON.parse(data);
         let responseTxt = "";
 
-        for ( const obj of data) {
-            console.log(obj);
+        for ( const obj of dataJSON) {
+            console.log("obj is : " + obj);
             responseTxt += obj.itemID + " : " + obj.itemName + " , ";
         }
 
