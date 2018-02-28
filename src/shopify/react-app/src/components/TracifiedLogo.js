88f76db7bfe266e10ebb2b0cc3e54dc51b9d618c
princessBuddy@123 <banurib@yahoo.com>
2018-02-28 12:11:26 +0530

    tracified logo size refinement...

--- src/shopify/react-app/src/components/TracifiedLogo.js
+++ src/shopify/react-app/src/components/TracifiedLogo.js
@@ -4,19 +4,23 @@ import TraciLogo from '../assets/tracified_logo.png';
 class TracifiedLogo extends Component{
     render(){
         var logoStyle={
-            height: '250px'             
+            // height: '250px'             
         }
 
         var backStyle={
             backgroundColor:"black",
-            textAlign: 'center',
-                         
-        
+            textAlign: 'center'
+        }
+
+        var wrapperStyle={
+            width:'100%'
         }
 
         return(
-            <div style={backStyle}>
-                <img src={TraciLogo} style={logoStyle}/>
+            <div style={wrapperStyle} >
+                <div style={backStyle}>
+                    <img src={TraciLogo} style={logoStyle}/>
+                </div>
             </div>
         );
     }
