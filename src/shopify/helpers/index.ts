d5f9ba63e3ba3f6e21b2c25aa40f7774e8e87897
DAMMA <dammakaru@gmail.com>
2018-03-04 15:40:25 +0530

    resolving issue with shopify api requests returning 400

--- src/shopify/helpers/index.ts
+++ src/shopify/helpers/index.ts
@@ -1,20 +1,20 @@
-    import * as crypto from "crypto";
-    import querystring = require("querystring");
-    import request = require("request-promise");
+import * as crypto from "crypto";
+import querystring = require("querystring");
+import request = require("request-promise");
 
-    export interface IHelper {
-        verifyQueryHMAC(query: any, apiSecret: string): boolean;
-        verifyPayloadHMAC(data: any, hmac: any, apiSecret: string, next: any): boolean;
-        shopAdminAPI(
-            method: string,
-            shop: any,
-            relUrl: string,
-            shopRequestHeaders: any,
-            body: any,
-            callback: (obj: any) => any): any;
-    }
+export interface IHelper {
+    verifyQueryHMAC(query: any, apiSecret: string): boolean;
+    verifyPayloadHMAC(data: any, hmac: any, apiSecret: string, next: any): boolean;
+    shopAdminAPI(
+        method: string,
+        shop: any,
+        relUrl: string,
+        shopRequestHeaders: any,
+        body: any,
+        callback: (obj: any) => any): any;
+}
 
-    export class Helper implements IHelper {
+export class Helper implements IHelper {
 
     public verifyQueryHMAC(query: any, apiSecret: string) {
         if (!query.hmac) {
@@ -34,8 +34,8 @@
     public verifyPayloadHMAC(data: any, hmac: any, apiSecret: string, next: any) {
         data = JSON.stringify(data);
         const digest = crypto.createHmac("SHA256", apiSecret)
-        .update(data)
-        .digest("base64");
+            .update(data)
+            .digest("base64");
         console.log("digest");
         console.log(digest);
         console.log("hmac");
@@ -45,15 +45,31 @@
 
     public shopAdminAPI(
         method: string, shop: any, relUrl: string, shopRequestHeaders: any, body: any, callback: (obj: any) => any) {
-        const options = {
-            body,
-            headers: shopRequestHeaders,
-            json: true,
-            method,
-            uri: "https://" + shop + relUrl,
-        };
+
+        let options;
+
+        if (body !== null) {
+            options = {
+                body,
+                headers: shopRequestHeaders,
+                json: true,
+                method,
+                uri: "https://" + shop + relUrl,
+            };
+
+        } else {
+
+            options = {
+                headers: shopRequestHeaders,
+                json: true,
+                method,
+                uri: "https://" + shop + relUrl,
+            };
+
+        }
+
         request(options).then(callback).catch((err: Error) => {
             return (err);
-          } );
+        });
     }
 }
