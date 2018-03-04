e0c50a8057121d7f73afd49b7e3360df867db6fc
DAMMA <dammakaru@gmail.com>
2018-03-04 16:27:37 +0530

    updated access token

--- src/shopify/routes/shop-api.ts
+++ src/shopify/routes/shop-api.ts
@@ -4,7 +4,7 @@ const router = Router();
 const shopAdminAPI = require("../helpers").shopAdminAPI;
 const shopName = "99xnsbm.myshopify.com";
 const shopRequestHeaders = {
-    "X-Shopify-Access-Token": "94687cbe08d0e4024c74f8adbffa94ca",
+    "X-Shopify-Access-Token": "b6445ab66c5f8372f19de5a4c7dcbf75",
 };
 
 // router.all("/*", (req: Request, res: Response, next: NextFunction) => {
