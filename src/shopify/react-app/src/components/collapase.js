9137e2fb235f5b3e803ede0e1341c4403c4898d1
princessBuddy@123 <banurib@yahoo.com>
2018-03-08 09:03:36 +0530

    fixing mark as tracified button

--- src/shopify/react-app/src/components/collapase.js
+++ src/shopify/react-app/src/components/collapase.js
@@ -88,7 +88,8 @@ class CollapaseCard extends Component {
                 <Stack alignment="baseline" wrap={false}> {resourceThumbnails} </Stack>
                 {/* </Collapse> */}
                     <Row>
-                        <Col sm="7">
+                        <Col sm="12">
+                        {/*<Col sm="7">*/}
                             <Row>
                                 <Col sm="4">
                                     <Button plain onClick={this.toggleCollapse} >{this.state.collapsed ? " Show Items \u25BC" : " Hide Items \u25B2"}</Button>
