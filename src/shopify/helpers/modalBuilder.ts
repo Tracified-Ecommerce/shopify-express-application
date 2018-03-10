1cb947ae326ddba94968581a484db6363e2375a5
princessBuddy@123 <banurib@yahoo.com>
2018-03-10 10:29:35 +0530

    handling validation of dimension content-error handling 1..

--- src/shopify/helpers/modalBuilder.ts
+++ src/shopify/helpers/modalBuilder.ts
@@ -178,12 +178,12 @@ function dimensionBuilder(dimensions: any): IDimensionJSON {
         dimensionComponents.htmltxt += "</td><td class=\"descript\" style=\"background-color:" + dimension.descriptionBackColor + ";color:" + dimension.descriptionTxtColor + "\">" + dimension.tagline + "</td></tr>";
 
         for (const x of dimension.data) {
-             if(x.value==null){
-                // document.getElementById()=""
-                x.label=null;
+            //  if(x.value==null){
+                var contentTitle=x.label;
+                // x.label=null;
                 // tslint:disable-next-line:max-line-length
-                dimensionComponents.htmltxt += "<tr class=\"dimensionContent\"><td class=\"keyTitle\">" + x.label + "</td><td class=\"keyContent\">" + x.value + "</td></tr>";
-             }
+                dimensionComponents.htmltxt += "<tr class=\"dimensionContent\"><td class=\"keyTitle\">" + contentTitle + "</td><td class=\"keyContent\">" + x.value + "</td></tr>";
+            //  }
         }
         dimensionComponents.htmltxt += "</table></div>";
     }
