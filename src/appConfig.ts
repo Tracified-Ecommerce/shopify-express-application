30a7cd8ccfb348379512e199ae5758d9b95b12f1
dammakaru <dammakaru@gmail.com>
2018-03-30 12:40:35 +0530

    changed api key and secret

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,8 +1,8 @@
 export const shopifyConfigs = {
     db: "mongodb://tracified:1234@ds219318.mlab.com:19318/ecommerceplugin",
     install: {
-        apiKey: "8cbed825a1a05c935cbb12574bb24257",
-        apiSecret: "4bc97ed0ae56f7e75f2560f7816fd96a",
+        apiKey: "a7958965f73ce0a23acbbed816bf6a5a",
+        apiSecret: "00f6a7d88f17eced8a4385c5aec93793",
         forwardingAddress: "https://tracified-api.herokuapp.com",
     },
     services: {
