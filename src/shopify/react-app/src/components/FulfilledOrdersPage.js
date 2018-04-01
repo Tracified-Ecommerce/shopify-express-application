6bdae967d381f545515bb13fb1a4dc6a2ee59438
princessBuddy@123 <banurib@yahoo.com>
2018-04-01 15:32:33 +0530

    properly added seperator keyword to fullfilledOrders Page

--- src/shopify/react-app/src/components/FulfilledOrdersPage.js
+++ src/shopify/react-app/src/components/FulfilledOrdersPage.js
@@ -208,7 +208,7 @@ class FulfilledOrdersPage extends Component {
                             }
 
             return (
-                <Page title="Tracified Orders" >
+                <Page title="Tracified Orders" seperator>
                      <div className="filterWrapper" style={filterStyle}>                       
                        <Stack alignment="center" >
                             <Stack.Item>
