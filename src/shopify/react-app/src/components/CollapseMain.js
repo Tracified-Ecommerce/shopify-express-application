122700a3f4cb7e9ebd05723df9a62d0b22584298
Banuri <banurib@yahoo.com>
2018-04-05 14:57:04 +0530

    UnTracified Collapsed View - CSS Bugs-alignment issues 3

--- src/shopify/react-app/src/components/CollapseMain.js
+++ src/shopify/react-app/src/components/CollapseMain.js
@@ -76,7 +76,7 @@ class CollapseMain extends Component {
                     <Col xs="3" sm="5" style={{ paddingTop: 5, paddingBottom: 5, paddingRight: 0, width: 409 }}>
                         <TextStyle variation="subdued"><strong>Customer:</strong> {this.props.order.customer}</TextStyle>
                     </Col>
-                    <Col className="exploreBtn" sm="2" style={{ paddingRight: 0, width: 130, display: table-cell, verticalAlign: "middle"}}>
+                    <Col className="exploreBtn" sm="2" style={{ paddingRight: 0, width: 130, display: "table-cell", verticalAlign: "middle"}}>
                         <Button
                             size="slim"
                             outline
