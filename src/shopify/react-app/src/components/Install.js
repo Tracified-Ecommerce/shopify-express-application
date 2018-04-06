9008babaffc4f839ceb03dfc4f3f377f28e77aaa
Banuri <banurib@yahoo.com>
2018-04-06 16:57:40 +0530

    fix the config page more

--- src/shopify/react-app/src/components/Install.js
+++ src/shopify/react-app/src/components/Install.js
@@ -15,17 +15,17 @@ class Installation extends Component {
         }
 
         return (
-            <Page title="wrapperInstall">
+            <Page title="Tracified - Configuration Instructions">
             <div>
-                <div class="InstallTitle">
+                {/* <div class="InstallTitle">
                     <h1>
                         <DisplayText size="medium">Tracified - Configuration Instructions</DisplayText>
                     </h1>
-                </div>
+                </div> */}
                 {/* <div class="InstallVideo"> */}
                     <iframe width="100%" height="583px" src="https://www.youtube.com/embed/gk8W521N5d4?rel=0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
                 {/* </div> */}
-                <div class="InstallDescription">
+                {/* <div class="InstallDescription"> */}
                     <Card>
                         <ol>
                             <li style={liStyle}>
@@ -62,7 +62,7 @@ class Installation extends Component {
                         </div>
                     </Card>
                     <br />
-                </div>
+                {/* </div> */}
             </div>
             </Page>
         );
