55e39e9114213f1c49f54a02631e73c6641bbc86
dammakaru <dammakaru@gmail.com>
2018-04-09 13:00:27 +0530

    changed db for tracified app

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,5 +1,5 @@
 export const shopifyConfigs = {
-    db: "mongodb://tracified:1234@ds219318.mlab.com:19318/ecommerceplugin",
+    db: "mongodb://tracified:123@ds139459.mlab.com:39459/tracified",
     install: {
         apiKey: "a7958965f73ce0a23acbbed816bf6a5a",
         apiSecret: "00f6a7d88f17eced8a4385c5aec93793",
