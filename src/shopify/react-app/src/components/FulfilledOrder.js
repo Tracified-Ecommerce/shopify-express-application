1f7cb4eb7c0d62f40a17733738730a73b8b78bb8
princessBuddy@123 <banurib@yahoo.com>
2018-04-11 21:41:01 +0530

    added selectWrapper class - FullfilledOrder.js

--- src/shopify/react-app/src/components/FulfilledOrder.js
+++ src/shopify/react-app/src/components/FulfilledOrder.js
@@ -156,13 +156,15 @@ class FulfilledOrder extends Component {
                     </div>
                 </td>
                 <td>
-                    <Select
-                        options={itemOptions}
-                        placeholder="Select an item"
-                        id={order.order_number}
-                        onChange={this.onSelectItem}
-                        value={this.state.itemEnable}
-                    />
+                    <div className="selectWrapper">
+                        <Select
+                            options={itemOptions}
+                            placeholder="Select an item"
+                            id={order.order_number}
+                            onChange={this.onSelectItem}
+                            value={this.state.itemEnable}
+                        />
+                    </div>
                 </td>
                 <td>
                     <div className="timelineBtnWrapper">
