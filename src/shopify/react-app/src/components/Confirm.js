2ca6fae2ea22e79a5c7fe65b88e3a0a58e371bfa
dammakaru <dammakaru@gmail.com>
2018-04-17 18:44:22 +0530

    confirm box z index

--- src/shopify/react-app/src/components/Confirm.js
+++ src/shopify/react-app/src/components/Confirm.js
@@ -13,7 +13,7 @@ class Confirm extends Component {
         position: 'fixed',
         top: 0,
         bottom: 0,
-        zIndex: 30,
+        zIndex: 200,
         left: 0,
         right: 0,
         backgroundColor: 'rgba(0,0,0,0.3)',
