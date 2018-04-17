a83717ff58ba0deadbb8f60a498e59078d445393
gallagehh@gmail.com <gallagehh@gmail.com>
2018-04-17 12:33:06 +0530

    css changes

--- src/shopify/react-app/src/components/Uncollapsed.js
+++ src/shopify/react-app/src/components/Uncollapsed.js
@@ -22,10 +22,12 @@ class Uncollapsed extends Component {
             <Card className="cardHeader" key={this.props.order.order_number} title={this.props.title} sectioned subdued={false}>
                 <Row id="unfulfillContent">    
                         <Col sm="10"  className="unfulfillColLeft" >
-                            <List type="bullet" id="untracified_list" >
-                                <List.Item>Customer  : {this.props.order.customer}</List.Item>
-                                <List.Item>Created At: {this.props.order.created_at}</List.Item>
-                            </List>
+                            <div className="list_col" >
+                                <List type="bullet" id="untracified_list" >
+                                    <List.Item>Customer  : {this.props.order.customer}</List.Item>
+                                    <List.Item>Created At: {this.props.order.created_at}</List.Item>
+                                </List>
+                            </div>
                         </Col>
                         <Col sm="3" className="QR_col" >
                             <QRCode className="QRcode" value={this.props.qrVal} />
