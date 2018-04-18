b93988f5ebfd232a56c055c3db490038e229bae8
dammakaru <dammakaru@gmail.com>
2018-04-18 17:15:52 +0530

    fixing the alert not seen bug

--- src/shopify/react-app/src/components/Alert.js
+++ src/shopify/react-app/src/components/Alert.js
@@ -14,7 +14,7 @@ class Alert extends Component {
         position: 'fixed',
         top: 0,
         bottom: 0,
-        zIndex: 30,
+        zIndex: 200,
         left: 0,
         right: 0,
         backgroundColor: 'rgba(0,0,0,0.3)',
