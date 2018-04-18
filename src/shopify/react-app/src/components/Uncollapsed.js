f85a44c58553650d97bb683b78e3dd380eed8306
dammakaru <dammakaru@gmail.com>
2018-04-18 19:18:00 +0530

    increasing font size for Untracified Orders card in mobiles

--- src/shopify/react-app/src/components/Uncollapsed.js
+++ src/shopify/react-app/src/components/Uncollapsed.js
@@ -5,6 +5,7 @@ import { Container, Row, Col } from 'reactstrap';
 import { Thumbnail, Card, Page, List } from '@shopify/polaris';
 import Loading from './Loading';
 import './untracifiedOrders_mediaQueries.css';
+import './Uncollapsed.css';
 const QRCode = require('qrcode.react');
 
 class Uncollapsed extends Component {
