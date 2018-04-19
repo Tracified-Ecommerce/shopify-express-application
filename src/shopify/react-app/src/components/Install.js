21bd2aae15ecc36675ee11af7203f5e29aa7d860
Banuri <banurib@yahoo.com>
2018-04-19 15:07:53 +0530

    mobile responsive the config page

--- src/shopify/react-app/src/components/Install.js
+++ src/shopify/react-app/src/components/Install.js
@@ -17,7 +17,7 @@ class Installation extends Component {
         return (
             <Page title="Tracified - Configuration Instructions">
             <div className="configWrapper">
-                    <iframe width="100%" height="583px" src="https://www.youtube.com/embed/gk8W521N5d4?showinfo=0&controls=1&rel=0&amp" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+                    <iframe className="ecomConfigVideo" width="100%" height="583px" src="https://www.youtube.com/embed/gk8W521N5d4?showinfo=0&controls=1&rel=0&amp" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
                
                     <Card>
                         <ol>
