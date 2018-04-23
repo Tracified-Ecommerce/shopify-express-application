e43e9fa072a42b17f910a5bceaf3bff65fd9e3de
Banuri <banurib@yahoo.com>
2018-04-23 11:49:55 +0530

    add styles to loading msg & spinner

--- src/shopify/react-app/src/components/Loading.js
+++ src/shopify/react-app/src/components/Loading.js
@@ -13,13 +13,25 @@ class Loading extends Component {
     }
 
     render() {
-       
+        var loadingMsgStyle={
+            marginLeft:"10%",
+            marginRight:"10%",
+            marginTop:"10%",
+            width:"80%",
+            color:"teal"
+        }
+
+        var spinnerStyle={
+            marginLeft:"30%",
+            marginRight:"30%"
+        }
+
         return (
-            <div className="loadingMsg">
-                <DisplayText  size="small">
+            <div className="loadingMsg" style={loadingMsgStyle}>
+                <DisplayText  size="small"                >
                     <TextStyle variation="subdued">{this.props.loadMsg}</TextStyle>
                 </DisplayText><br />
-                <div className="spinnerClass" >
+                <div className="spinnerClass" style={spinnerStyle}>
                     <Spinner
                         size="large"
                         color="teal"
